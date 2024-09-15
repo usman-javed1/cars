f12bd245000ecf3508a50372458ab8d6501b2ffb
AndriiMandziuk <edwardthomas7770@gmail.com>
2024-09-15 12:46:05 +0800

    add routes & page structure

--- src/App.js
+++ src/App.js
@@ -1,29 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
+import { Route, Routes } from "react-router-dom";
+
 import Navbar from './components/General/Navbar';
-import Card from './components/General/Card';
-import HeroSection from './components/main/HeroSection/HeroSection';
-import Features from './components/main/Features/Features';
-import Details from './components/main/Details/Details';
-import Categories from './components/main/Categories/Categories';
-import HowWorks from './components/main/HowWorks/HowWorks';
-import FAQ from './components/main/FAQ/FAQ';
-import Explore from './components/main/Explore/Explore';
 import Footer from './components/General/Footer';
+
+//pages
+import LandingPage from './pages/Landingpage';
+import About from './pages/About';
+import Blog from './pages/Blog';
+import CarDetails from './pages/CarDetails';
+import FAQ from './pages/FAQ';
+import Financing from './pages/Financing';
+import News from './pages/News';
+import OurTeam from './pages/OurTeam';
+import SpecialDeals from './pages/SpecialDeals';
+import ViewCars from './pages/ViewCars';
 
 function App() {
   return (
     <div className="App w-[100%] max-w-full mx-auto">
       <Navbar />
-      <HeroSection />
-      <div className="w-full max-w-full mx-auto">
-        <Features />
-        <Details />
-        <Categories />
-        <HowWorks />
-        <FAQ />
-      </div>
-      <Explore />
+      <Routes>
+        <Route path='/' element={<LandingPage/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='/blog' element={<Blog/>} />
+        <Route path='/faq' element={<FAQ/>} />
+        <Route path='/financing' element={<Financing/>} />
+        <Route path='/news' element={<News/>} />
+        <Route path='/our-team' element={<OurTeam/>} />
+        <Route path='/special-deals' element={<SpecialDeals/>} />
+        <Route path='/view' element={<ViewCars/>} />
+        <Route path='/detail/:id' element={<LandingPage/>} />
+      </Routes>
       <Footer />
     </div>
   );
