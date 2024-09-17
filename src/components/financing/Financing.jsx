8d6bdcad1aae87797bf6deec975bee438aec6601
Usman <126940389+NajamJaved@users.noreply.github.com>
2024-09-17 16:23:19 +0500

    Helllo

---
+++ src/components/financing/Financing.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ContactForm from  '../ContactForm/ContactForm'
+
+const Financing = () => {
+    return (
+        <div>
+            <ContactForm />
+        </div>
+    )
+}
+
+export default Financing
--- src/pages/Financing.js
+++ src/pages/Financing.js
@@ -1,10 +1,17 @@
 import React from "react";
 import ContactForm from "../components/ContactForm/ContactForm";
+import ContainerWraper from "../components/General/ContainerWraper";
+import Explore from "../components/main/Explore/Explore";
 
 const Financing = () => {
-  return(
+  return (
     <>
-      <ContactForm></ContactForm>
+      <div className="w-full max-w-full mx-auto flex flex-col justify-between items-center">
+        <ContainerWraper>
+          {/* <ContactForm></ContactForm> */}
+        </ContainerWraper>
+        <Explore />
+      </div>
     </>
   );
 };
