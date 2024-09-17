e375f22155319ebd374cefaca37da6a2478e55a5
Usman <126940389+NajamJaved@users.noreply.github.com>
2024-09-17 13:55:06 +0500

    Push

--- src/components/Blog/MainBlog/MainBlog.jsx
+++ src/components/Blog/MainBlog/MainBlog.jsx
@@ -9,7 +9,7 @@ const MainBlog = () => {
         </div>
         <div className="w-[443px]  m-auto lg:mt-0 md:mt-0 mt-10">
           <p className='text-[14px]  font-[500] text-[#959595]'>New Models and Offers</p>
-          <h2 className='lg:class-for-h2 text-[18px] font-[700] py-[20px]'>
+          <h2 className='lg:text-[36px] text-[18px] font-[700] py-[20px]'>
             New Models Available for Leasing!
           </h2>
           <p className='lg:text-[14px] text-[12px]  font-[500] text-[#959595] lg:w-[444px] w-[325px] '>We are excited to announce the addition of the latest models from top brands to our leasing options! Discover the new Mercedes-Benz, BMW, Audi, and Tesla models now available for lease.</p>
