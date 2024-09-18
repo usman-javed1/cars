d42aa9b943163a9459abf46de1f528031e1c3587
AndriiMandziuk <edwardthomas7770@gmail.com>
2024-09-18 23:40:51 +0800

    update routes

--- src/components/General/Navbar.jsx
+++ src/components/General/Navbar.jsx
@@ -22,13 +22,13 @@ const Navbar = () => {
     ];
 
     const dropdownLinks = [
-        { text: 'About us', image: require('../../images/arrow.png') },
-        { text: 'Our Team', image: require('../../images/arrow.png') },
-        { text: 'Facebook', image: require('../../images/darkfb.png') },
-        { text: 'Youtube', image: require('../../images/darkyoutube.png') },
-        { text: 'Telegram', image: require('../../images/darktele.png') },
-        { text: 'Instagram', image: require('../../images/darkinsta.png') },
-        { text: 'Whatsapp', image: require('../../images/darkwhatsapp.png') },
+        { text: 'About us', image: require('../../images/arrow.png'), link: '/about' },
+        { text: 'Our Team', image: require('../../images/arrow.png'), link: '/our-team' },
+        { text: 'Facebook', image: require('../../images/darkfb.png'), link: '/' },
+        { text: 'Youtube', image: require('../../images/darkyoutube.png'), link: '/' },
+        { text: 'Telegram', image: require('../../images/darktele.png'), link: '/' },
+        { text: 'Instagram', image: require('../../images/darkinsta.png'),link: '/' },
+        { text: 'Whatsapp', image: require('../../images/darkwhatsapp.png'),link: '/' },
     ];
 
     return (
@@ -36,9 +36,9 @@ const Navbar = () => {
             <nav className='w-[100%] max-w-full lg:h-[80px] h-[70px] items-center lg:justify-between justify-between flex bg-black relative lg:pr-0 pr-5 lg:px-[60px] px-[25px] ' style={{position: showMenu ? "fixed": "relative", zIndex: 99999}}>
                 
                 <div className="logo">
-                    <a href='/'>
+                    <Link to='/'>
                         <img src={require("../../images/logo.png")} alt="logo" width={142} height={42} />
-                    </a>
+                    </Link>
                 </div>
                 <div className="h-[56px]  text-white items-center text-[16px] justify-between lg:flex hidden w-[47%]">
                     <ul className='flex w-full justify-between items-center font-[500]' >
