00b406276606d51e38c96d331903d43932975c8b
Usman <126940389+NajamJaved@users.noreply.github.com>
2024-09-20 21:56:46 +0500

    Push

--- src/components/CarDetail/main/CarDetailsHero.jsx
+++ src/components/CarDetail/main/CarDetailsHero.jsx
@@ -2,22 +2,22 @@ import React, { useState } from 'react';
 
 const CarDetailsHero = () => {
     const images = [
-        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"), 
-        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"), 
-        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"), 
-        
+        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"),
+        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"),
+        require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png"),
+
         require("../../../images/315_isolated_realistic_metallic_white_high_performance_racing_super_car_from_left_side_view 1 (1).png")
     ];
 
     const [selectedImage, setSelectedImage] = useState(images[0]); // Set the initial image
 
     return (
-        <div className="mb-20">
-            <div className='lg:h-[473px] h-[699px] bg-[#383838] w-full lg:flex-row flex-col rounded-[38px] mb-10 flex lg:justify-between items-center relative'>
+        <div className="lg:mb-20 mb-32">
+            <div className='lg:h-[493px] h-[739px] bg-[#383838] w-full lg:flex-row flex-col rounded-[38px] mb-10 flex lg:justify-between items-center relative'>
                 <div className="imageSection lg:-ml-[90px] -ml-3 lg:mt-0 mt-10">
                     <img src={selectedImage} alt="Selected Car" className='lg:w-[738px] w-[370px] lg:h-[280px] h-[140px]' />
                 </div>
-                <div className="content px-[5%] lg:mt-0 mt-14">
+                <div className="content px-[6%] lg:ml-0 ml-2 lg:mt-0 mt-14 lg:justify-start justify-center">
                     <div className="tag text-[#959595] text-[14px] font-[500]">
                         Sport cars
                     </div>
@@ -38,7 +38,7 @@ const CarDetailsHero = () => {
                         1239$ <span className='text-[12px] font-[500]'>/ month</span>
                     </div>
 
-                    <div className="lg:absolute bottom-5 right-5">
+                    <div className="lg:absolute bottom-10 right-10">
                         <div className="lg:w-[112px] w-[277px] h-[39px] flex justify-between items-center rounded-[10px] border border-[#959595] px-2 mt-2">
                             <div className="text-[#fff] font-[500] text-[12px]">
                                 4 Seats
@@ -60,19 +60,19 @@ const CarDetailsHero = () => {
                     </div>
 
                     <div className="button  lg:mt-5 mt-10">
-                        <button className='lg:w-[151px] w-[277px] h-[56px] flex justify-center items-center p-[22px, 18px, 18px, 18px] rounded-[10px] bg-white text-black text-[16px] font-[500] transition-all duration-300 hover:bg-[#F6B000] hover:text-white'>
-                            Request a Call
+                        <button className='lg:w-[251px] w-[277px] h-[56px] flex justify-center items-center p-[22px, 18px, 18px, 18px] rounded-[10px] bg-white text-black text-[16px] font-[500] transition-all duration-300 hover:bg-[#F6B000] hover:text-white'>
+                            Request a quote
                         </button>
                     </div>
                 </div>
             </div>
-            
-            <div className="grid-cols-4 lg:grid hidden">
+
+            <div className=" lg:flex justify-between hidden">
                 {images.map((image, index) => (
                     <div
                         key={index}
                         onClick={() => setSelectedImage(image)} // Update the selected image when clicked
-                        className="w-[295px] cursor-pointer h-[240px] bg-lightgray rounded-[30px] flex justify-center items-center"
+                        className="w-[24%] cursor-pointer h-[240px] bg-lightgray rounded-[30px] flex justify-center items-center"
                     >
                         <img src={image} alt={`Car ${index + 1}`} className='w-[243px] h-[93px]' />
                     </div>
