a9553f09f0b6dd23222c73c3563420e505aebccf
Usman <126940389+NajamJaved@users.noreply.github.com>
2024-09-21 14:17:58 +0500

    Push

--- src/components/AboutUs/AboutUsHero/AboutUsHero.jsx
+++ src/components/AboutUs/AboutUsHero/AboutUsHero.jsx
@@ -2,6 +2,20 @@ import React from 'react'
 import Breadcrumb from '../../Blog/Breadcrumb/Breadcrumb';
 import ContainerWraper from '../../General/ContainerWraper';
 const AboutUsHero = ({ heading = "About Us: The Wheel Deal", desc = "Welcome to The Wheel Deal, your trusted partner in car leasing. We are dedicated to offering you the best deals, the newest models, and a seamless leasing experience tailored to your needs.", btnContent = "Contact us", imageUrl, breadCrumbs, activeCrumb, isSpecial = false }) => {
+
+  const scrollToBottomOffset = () => {
+    // const offsetFromBottom = 1142;
+    // Calculate the position to scroll to
+    const isMobile = window.innerWidth < 786;
+    const offsetFromBottom = isMobile ? 2100 : 1142
+    const scrollToPosition = document.body.scrollHeight - offsetFromBottom;
+
+    // Scroll to the calculated position smoothly
+    window.scrollTo({
+      top: scrollToPosition,
+      behavior: 'smooth'
+    });
+  };
   return (
     <>
 
@@ -21,13 +35,13 @@ const AboutUsHero = ({ heading = "About Us: The Wheel Deal", desc = "Welcome to 
               </div>
               <p className='text-[14px]  font-[500] text-[#B9B9B9] lg:w-[400px] w-[100%] '>{desc} </p>
             </div>
-            <a href='#footer212'>
-              <div className="button pt-[45px]">
+            
+              <div className="button pt-[45px]" onClick={scrollToBottomOffset}>
                 <button className='w-[151px] h-[56px]  flex justify-center items-center p-[22px, 18px, 18px, 18px] rounded-[10px] bg-white text-black text-[16px] font-[500] transition-all duration-300 hover:bg-[#F6B000] hover:text-white'>
                   {btnContent}
                 </button>
               </div>
-            </a>
+            
           </div>
         </ContainerWraper>
       </div>
