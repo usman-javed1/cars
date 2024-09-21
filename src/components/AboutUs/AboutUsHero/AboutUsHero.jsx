8ac7680060e585b38b45ed54b233aead9b5a5886
Usman <126940389+NajamJaved@users.noreply.github.com>
2024-09-21 10:07:17 +0500

    Done

--- src/components/AboutUs/AboutUsHero/AboutUsHero.jsx
+++ src/components/AboutUs/AboutUsHero/AboutUsHero.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Breadcrumb from '../../Blog/Breadcrumb/Breadcrumb';
 import ContainerWraper from '../../General/ContainerWraper';
-const AboutUsHero = ({ heading = "About Us: The Wheel Deal", desc = "Welcome to The Wheel Deal, your trusted partner in car leasing. We are dedicated to offering you the best deals, the newest models, and a seamless leasing experience tailored to your needs.", btnContent = "Contact us", imageUrl, breadCrumbs, activeCrumb, isSpecial=false }) => {
+const AboutUsHero = ({ heading = "About Us: The Wheel Deal", desc = "Welcome to The Wheel Deal, your trusted partner in car leasing. We are dedicated to offering you the best deals, the newest models, and a seamless leasing experience tailored to your needs.", btnContent = "Contact us", imageUrl, breadCrumbs, activeCrumb, isSpecial = false }) => {
   return (
     <>
 
@@ -21,11 +21,13 @@ const AboutUsHero = ({ heading = "About Us: The Wheel Deal", desc = "Welcome to 
               </div>
               <p className='text-[14px]  font-[500] text-[#B9B9B9] lg:w-[400px] w-[100%] '>{desc} </p>
             </div>
-            <div className="button pt-[45px]">
-              <button className='w-[151px] h-[56px]  flex justify-center items-center p-[22px, 18px, 18px, 18px] rounded-[10px] bg-white text-black text-[16px] font-[500] transition-all duration-300 hover:bg-[#F6B000] hover:text-white'>
-                {btnContent}
-              </button>
-            </div>
+            <a href='#footer212'>
+              <div className="button pt-[45px]">
+                <button className='w-[151px] h-[56px]  flex justify-center items-center p-[22px, 18px, 18px, 18px] rounded-[10px] bg-white text-black text-[16px] font-[500] transition-all duration-300 hover:bg-[#F6B000] hover:text-white'>
+                  {btnContent}
+                </button>
+              </div>
+            </a>
           </div>
         </ContainerWraper>
       </div>
